6553d23449f26b6859134126316d1c8e81c4477b
Pay10 <91730659+rohitpay10@users.noreply.github.com>
2021-11-18 22:40:00 +0530

    Add files via upload

---
+++ catalog/language/english/payment/payten.php
@@ -0,0 +1,13 @@
+<?php
+// Text
+$_['text_title']            = 'Credit Card / Debit Card (Pay10)';
+$_['text_unable']           = 'Unable to locate or update your order status';
+$_['text_declined']         = 'Payment was declined by Pay10';
+$_['text_failed']           = 'Pay10 Transaction Failed';
+$_['text_failed_message']   = '<p>Unfortunately there was an error processing your Pay10 transaction.</p><p><b>Warning: </b>%s</p><p>Please verify your Pay10 account balance before attempting to re-process this order</p><p> If you believe this transaction has completed successfully, or is showing as a deduction in your Pay10 account, please <a href="%s">Contact Us</a> with your order details.</p>';
+$_['text_basket']           = 'Basket';
+$_['text_checkout']         = 'Checkout';
+$_['text_success']          = 'Success';
+$_['button_confirm']          = 'Confirm';
+$_['heading_title']				= 'Thank you for shopping with us .... ';
+$_['button_continue']            = 'Continue';
